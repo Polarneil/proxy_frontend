452eb6c1a1190b137bc8651931dc600b6a973f39
Neil <neilwl022@gmail.com>
2025-03-18 16:28:10 -0500

    styling updates

--- src/modules/app/App.js
+++ src/modules/app/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import Loading from '../common/components/loading';
 import AuthenticatedContent from '../common/pages/AuthenticatedContent';
+import logo from '../common/img/logo.svg';
 
 function App() {
-  const { isLoading } = useAuth0();
+  const { isLoading, user } = useAuth0();
 
   if (isLoading) {
     return <Loading />;
@@ -12,6 +13,14 @@ function App() {
 
   return (
     <div>
+      <header style={{ backgroundColor: '#f8f5f2', padding: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', borderBottom: '1px solid #ccc' }}>
+        <img src={logo} alt="Logo" style={{ height: '40px' }} />
+        {user && (
+          <div style={{ borderRadius: '50%', width: '40px', height: '40px', backgroundColor: '#ccc', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '16px' }}>
+            {user.name.split(" ").map(word => word[0]).join("").toUpperCase()}
+          </div>
+        )}
+      </header>
       <AuthenticatedContent />
     </div>
   );
